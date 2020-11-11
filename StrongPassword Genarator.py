857cee873206ae4b4cba594fa9fd5291b2a619ee
Anu-Ansaf <68994600+Anu-Ansaf@users.noreply.github.com>
2020-11-11 14:26:33 +0530

    Update StrongPassword Genarator.py

--- StrongPassword Genarator.py
+++ StrongPassword Genarator.py
@@ -2,6 +2,7 @@ import random
 
 s = 'abcdefghijklmnpoqrstuvwxyz1234567890ABCDEFGHIJKLMNPOQSRTUVWXYZ!@#$%^&*()_+'
 
-LengthOfPass = 10
+LengthOfPass = 14
 p = ''.join(random.sample(s, LengthOfPass))
 print("Your Password is : " + p)
+print("Password Length of :" +  str(LengthOfPass))
